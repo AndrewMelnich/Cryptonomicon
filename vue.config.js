185d87d63201a918cacfc926f05342e55dbd25cd
Andrew Melnichenko <andrew.melnich@yandex.ru>
2023-03-16 13:57:48 +0700

    change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/cryptonomicon/',
   transpileDependencies: true,
-  publicPath: '/cryptonomicon/'
 })
